0ae1a490aa10b7fe7103b455a577662e92de5e3d
Jake <javakatdesign@gmail.com>
2014-04-23 20:35:33 -0500

    Fix that fix

--- server.cpp
+++ server.cpp
@@ -245,7 +245,7 @@ Packet createPacket(int index){
     cout << "=== TRANSMISSION START" << endl;
     string mstr = fstr.substr(index * BUFSIZE, BUFSIZE);
     //if(index * BUFSIZE + BUFSIZE > length) {
-	if(mstr.length < BUFSIZE) {
+	if(mstr.length() < BUFSIZE) {
 	  cout << "mstr, before null char: " << endl << mstr << endl;
 	  cout << "Starting position: " << index * BUFSIZE + BUFSIZE << endl;
 	  cout << "Length of file: " << length << endl;
