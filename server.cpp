eb6681fe583cab6234a7cd7c9b1c32be963a6b63
Jake <javakatdesign@gmail.com>
2014-04-23 21:16:09 -0500

    Fix the delay fix

--- server.cpp
+++ server.cpp
@@ -264,10 +264,8 @@ bool sendPacket(){
     int pc = probCorrupt; int pl = probLoss; int pd = probDelay;
 	bool* pckStatus = gremlin(&p, pc, pl, pd);
 
-	if (dropPck = pckStatus[0]) return false;
-	if (delayPck = pckStatus[1]) {
-		sleep(delayT);
-	}
+	if ((dropPck = pckStatus[0]) == true) return false;
+	if ((delayPck = pckStatus[1]) == true) sleep(delayT);
     if(sendto(s, p.str(), BUFSIZE + 7, 0, (struct sockaddr *)&ca, sizeof(ca)) < 0) {
 		cout << "Package sending failed. (socket s, server address sa, message m)" << endl;
 		return false;
