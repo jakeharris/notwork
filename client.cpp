304f81d25207706358e74d351a631a04057d7a5e
Jake <javakatdesign@gmail.com>
2014-04-24 22:51:24 -0500

    Add test statement

--- client.cpp
+++ client.cpp
@@ -328,6 +328,7 @@ bool getFile(){
 	  string wbs = to_string((long long)base);
 	  const char * ackval = wbs.c_str();
 	  cout << "wbs: " << wbs << endl;
+	  cout << "ackval: " << ackval << endl;
 
       if(sendto(s, ackval, 5, 0, (struct sockaddr *)&sa, salen) < 0) {
         cout << "Acknowledgement failed. (socket s, acknowledgement message ack, client address ca, client address length calen)" << endl;
