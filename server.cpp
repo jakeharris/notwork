36360f059483b460b464cc86f83943c76ecb536f
Jake <javakatdesign@gmail.com>
2014-04-25 02:47:20 -0500

    Same as previous

--- server.cpp
+++ server.cpp
@@ -238,7 +238,7 @@ void loadWindow(){
 		window[i-base] = createPacket(i);
 		if(strlen(window[i-base].getDataBuffer()) < BUFSIZE - 1) { 
 			for(++i; i < base + WIN_SIZE; i++){
-				delete window[i-base];
+				window[i-base] = 0;
 			}
 			break;
 		}
