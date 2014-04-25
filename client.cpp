b13f6cf8d25ede6a4683c06b868a785986b0f9a0
Jake <javakatdesign@gmail.com>
2014-04-25 04:10:34 -0500

    ?!?!?!?!

--- client.cpp
+++ client.cpp
@@ -275,9 +275,7 @@ bool getFile(){
     unsigned char dataPull[BUFSIZE];
     rlen = recvfrom(s, packet, PAKSIZE + 1, 0, (struct sockaddr *)&sa, &salen);
 
-	string pkstr (&packet);
-
-	if(pkstr[0] == "\0") break;
+	if(packet == '\0') break;
 
 	for(int x = 0; x < BUFSIZE; x++) {
       dataPull[x] = packet[x + 8];
