b9f59265116500b1bab2438746170c5d04d4609d
Jake <javakatdesign@gmail.com>
2014-04-24 22:40:07 -0500

    Make the error human-readable
    this is like the best thing I've seen in C++

--- client.cpp
+++ client.cpp
@@ -330,7 +330,7 @@ bool getFile(){
 
       if(sendto(s, ackval, PAKSIZE, 0, (struct sockaddr *)&sa, salen) < 0) {
         cout << "Acknowledgement failed. (socket s, acknowledgement message ack, client address ca, client address length calen)" << endl;
-		cout << "errno: " << errno << endl;
+		cout << strerr(errno) << endl;
         return 0;
       }
 	  delete sns;
