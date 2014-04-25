435e3f654aec399f68a573775b46deae4f2f63c0
Jake <javakatdesign@gmail.com>
2014-04-25 06:08:30 -0500

    ;;;;;;-;

--- server.cpp
+++ server.cpp
@@ -236,7 +236,7 @@ bool loadFile() {
 void loadWindow(){
 	for(int i = base; i < base + WIN_SIZE; i++) {
 		window[i-base] = createPacket(i);
-		if(strlen(window[i-base].getDataBuffer()) < BUFSIZE - 1 && window[i-base].chksm()) { 
+		if(strlen(window[i-base].getDataBuffer()) < BUFSIZE && window[i-base].chksm()) { 
 			cout << "In loadWindow's secret base, index is: " << i-base << endl;
 			for(++i; i < base + WIN_SIZE; i++){
 				window[i-base].loadDataBuffer("\0");
