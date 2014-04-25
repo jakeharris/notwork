4b73c033719f74d289e2aded223ba195a82f18d0
Jake <javakatdesign@gmail.com>
2014-04-25 05:52:23 -0500

    ;-;;;;;;

--- server.cpp
+++ server.cpp
@@ -237,6 +237,7 @@ void loadWindow(){
 	for(int i = base; i < base + WIN_SIZE; i++) {
 		window[i-base] = createPacket(i);
 		if(strlen(window[i-base].getDataBuffer()) < BUFSIZE - 1 && window[i-base].chksm()) { 
+			cout << "In loadWindow's secret base, index is: " << i-base << endl;
 			for(++i; i < base + WIN_SIZE; i++){
 				window[i-base].loadDataBuffer("\0");
 			}
