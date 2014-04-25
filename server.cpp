6b4ebcca1cf7c45ddd8a62a62ba8fd524c08acce
Jake <javakatdesign@gmail.com>
2014-04-25 08:09:46 -0500

    *ahem*

--- server.cpp
+++ server.cpp
@@ -276,6 +276,8 @@ bool sendFile() {
 		}
 		for(int x = 0; x < WIN_SIZE; x++) {
 			FD_ZERO(&stReadFDS);
+			stTimeOut.tv_sec = 1;
+			stTimeOut.tv_usec = 1000 * TIMEOUT;
 			FD_SET(0, &stReadFDS);
 			cout << "begin loop no. " << x << endl;
 			int t = select(1, &stReadFDS, NULL, NULL, &stTimeOut);
