a6a76780407d912b3a6104a3df433f73bfd3b4d7
Jake <javakatdesign@gmail.com>
2014-04-24 20:43:25 -0500

    T_T

--- client.cpp
+++ client.cpp
@@ -316,9 +316,7 @@ bool getFile(){
 
 	  if(packet[6] == '1') usleep(delayT*1000);
 
-	  int * ackval = windowBase;
-
-      if(sendto(s, ackval, PAKSIZE, 0, (struct sockaddr *)&sa, salen) < 0) {
+      if(sendto(s, *windowBase, PAKSIZE, 0, (struct sockaddr *)&sa, salen) < 0) {
         cout << "Acknowledgement failed. (socket s, acknowledgement message ack, client address ca, client address length calen)" << endl;
         return 0;
       }
