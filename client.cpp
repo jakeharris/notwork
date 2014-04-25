bd92f1a9d81404676fb6256dcb1db0df376e8b5d
Jake <javakatdesign@gmail.com>
2014-04-24 22:15:55 -0500

    Fix test statement (whoops paren-error!)

--- client.cpp
+++ client.cpp
@@ -263,7 +263,7 @@ bool isvpack(unsigned char * p) {
   cout << "base: " << base << endl;
   cout << "base + WIN_SIZE: " << base + WIN_SIZE << endl;
   cout << "sn >= base: " << (sn >= base) << endl;
-  cout << "sn < (base + WIN_SIZE): " << (sn < (base + WIN_SIZE) << endl;
+  cout << "sn < (base + WIN_SIZE): " << (sn < (base + WIN_SIZE)) << endl;
 
   if((sn >= base) && (sn < (base + WIN_SIZE))) { cout << "how is this real life" << endl; return false; }
 
